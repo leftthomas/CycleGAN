f88bcdadd7f297f8cf38f464ac1a6cd556cc2879
leftthomas <leftthomas@qq.com>
2021-03-02 16:13:37 +0800

    fix bug

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def train(g_a, g_b, d_a, d_b, data_loader, g_optimizer, da_optimizer, db_optimiz
         generators_loss = adversarial_loss + 10 * cycle_loss + 5 * identity_loss
         generators_loss.backward()
         g_optimizer.step()
-        total_g_loss += generators_loss.item() * batch_size
+        total_g_loss += generators_loss.item() * real_a.size(0)
 
         # Discriminator A #
         da_optimizer.zero_grad()
@@ -67,7 +67,7 @@ def train(g_a, g_b, d_a, d_b, data_loader, g_optimizer, da_optimizer, db_optimiz
                             + criterion_adversarial(pred_fake_a, target_fake_a)) / 2
         adversarial_loss.backward()
         da_optimizer.step()
-        total_da_loss += adversarial_loss.item() * batch_size
+        total_da_loss += adversarial_loss.item() * real_a.size(0)
 
         # Discriminator B #
         db_optimizer.zero_grad()
@@ -80,9 +80,9 @@ def train(g_a, g_b, d_a, d_b, data_loader, g_optimizer, da_optimizer, db_optimiz
                             + criterion_adversarial(pred_fake_b, target_fake_b)) / 2
         adversarial_loss.backward()
         db_optimizer.step()
-        total_db_loss += adversarial_loss.item() * batch_size
+        total_db_loss += adversarial_loss.item() * real_a.size(0)
 
-        total_num += batch_size
+        total_num += real_a.size(0)
         train_bar.set_description('Train Epoch: [{}/{}] G Loss: {:.4f}, DA Loss: {:.4f}, DB Loss: {:.4f}'
                                   .format(epoch, epochs, total_g_loss / total_num, total_da_loss / total_num,
                                           total_db_loss / total_num))
@@ -98,8 +98,8 @@ def val(g_a, g_b, data_loader):
         for a, b, a_name, b_name in tqdm(data_loader, desc='Generating images', dynamic_ncols=True):
             fake_b = (g_a(a.cuda()) + 1.0) / 2
             fake_a = (g_b(b.cuda()) + 1.0) / 2
-            save_a_path = '{}/A/{}'.format(save_root, a_name.split('/')[-1])
-            save_b_path = '{}/B/{}'.format(save_root, b_name.split('/')[-1])
+            save_a_path = '{}/A/{}'.format(save_root, a_name[0].split('/')[-1])
+            save_b_path = '{}/B/{}'.format(save_root, b_name[0].split('/')[-1])
             save_image(fake_b, save_a_path)
             save_image(fake_a, save_b_path)
 
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     train_data = ImageDataset(data_root, 'train')
     test_data = ImageDataset(data_root, 'test')
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=8)
-    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=8)
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=False, num_workers=8)
 
     # model setup
     G_A = Generator(3, 3).cuda()
